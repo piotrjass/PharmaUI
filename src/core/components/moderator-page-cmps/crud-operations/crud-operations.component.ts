233cd89b364cfc2625476626f5f508fd72052a04
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-07-21 14:01:15 +0200

    Working on crud operations

--- src/core/components/moderator-page-cmps/crud-operations/crud-operations.component.ts
+++ src/core/components/moderator-page-cmps/crud-operations/crud-operations.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-crud-operations',
@@ -12,4 +12,14 @@ export class CrudOperationsComponent {
   @Input() add_title: string = '';
   @Input() delete_title: string = '';
   @Input() items: string[] = [];
+
+  @Output() addItem = new EventEmitter<string>();
+  @Output() deleteItem = new EventEmitter<string>();
+
+  onAddItem(item: string) {
+    this.addItem.emit(item);
+  }
+  onDeleteItem(item: string) {
+    this.deleteItem.emit(item);
+  }
 }
