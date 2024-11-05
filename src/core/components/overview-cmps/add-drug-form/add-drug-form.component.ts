cae333124a14b17249a964ffb547899c4e5bf920
Piotr <piotrjasiulewiczkontakt@gmail.com>
2024-11-06 00:15:06 +0100

    Hide the add drug form

--- src/core/components/overview-cmps/add-drug-form/add-drug-form.component.ts
+++ src/core/components/overview-cmps/add-drug-form/add-drug-form.component.ts
@@ -5,18 +5,27 @@ import { SingleDrugComponent } from '../single-drug/single-drug.component';
 import {
   bootstrapPatchMinusFill,
   bootstrapPatchPlusFill,
+  bootstrapArrowDownCircleFill,
 } from '@ng-icons/bootstrap-icons';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-add-drug-form',
   standalone: true,
-  imports: [NgIconComponent],
+  imports: [NgIconComponent, CommonModule],
   templateUrl: './add-drug-form.component.html',
   styleUrl: './add-drug-form.component.css',
   viewProviders: [
     provideIcons({
       bootstrapPlusLg,
       bootstrapPatchPlusFill,
+      bootstrapArrowDownCircleFill,
     }),
   ],
 })
-export class AddDrugFormComponent {}
+export class AddDrugFormComponent {
+  showAddDrugSection: boolean = true;
+
+  toggleShowAddDrugSection() {
+    this.showAddDrugSection = !this.showAddDrugSection;
+  }
+}
